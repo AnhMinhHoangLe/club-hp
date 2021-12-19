b36afa9074b173fa08cc80097ce130da0669214a
= <=>
2021-12-19 15:59:11 -0800

    add-bootstrap

--- src/Components/homepage/past-events/nav-tab/navTab.component.jsx
+++ src/Components/homepage/past-events/nav-tab/navTab.component.jsx
@@ -1,24 +1,6 @@
-import React from 'react';
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
+import React from "react";
 
-
-
-const NavTab = ({onRouteChange, activeTab}) => {
-        return (
-            
-                <div>
-                         <Tabs defaultActiveKey={activeTab} id="uncontrolled-tab-example" className="mb-3">
-                                <Tab eventKey="tab1" title="Industry Events" onClick={() => onRouteChange('tab1')}>
-                                </Tab>
-                                <Tab eventKey="tab2" title="Socials" onClick={() => onRouteChange('tab2')}>
-                                        </Tab>
-                                <Tab eventKey="tab3" title="Workshops" onClick={() => onRouteChange('tab3')}>
-                                </Tab>
-                                </Tabs>
-       </div>
-
-
-    )
-}
+const NavTab = ({ onRouteChange, activeTab }) => {
+  return <div></div>;
+};
 export default NavTab;
